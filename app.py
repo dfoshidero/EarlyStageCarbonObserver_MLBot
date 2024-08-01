095194848c244ee71b90284f47ba52aa4bb818ec
Favour <dfoshidero@outlook.com>
2024-08-01 15:41:53 +0100

    changed port

--- app.py
+++ app.py
@@ -220,5 +220,5 @@ def predict(
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(debug=True, host="0.0.0.0", port=port)
